7e9b49e0fed6a31c95ad359202dbd55cafa1d921
Dchole <derekoware47@gmail.com>
2020-05-12 16:58:38 +0000

    Redesign member form

--- client/src/components/Forms/MemberForm.js
+++ client/src/components/Forms/MemberForm.js
@@ -1,10 +1,12 @@
 import React, { useState, useContext, useEffect } from "react"
+import { makeStyles } from "@material-ui/core/styles"
 import Button from "@material-ui/core/Button"
 import TextField from "@material-ui/core/TextField"
 import Paper from "@material-ui/core/Paper"
 import Typography from "@material-ui/core/Typography"
 import CircularProgress from "@material-ui/core/CircularProgress"
 import Backdrop from "@material-ui/core/Backdrop"
+import Container from "@material-ui/core/Container"
 import Grid from "@material-ui/core/Grid"
 import Alert from "@material-ui/lab/Alert"
 import { ListContext } from "../../context/ListContext"
@@ -15,17 +17,27 @@ import io from "socket.io-client"
 
 const socket = io("localhost:5000")
 
-const alertStyle = {
-  position: "absolute",
-  top: 0,
-  width: "100%",
-  display: "flex",
-  justifyContent: "center",
-  zIndex: 50000
-}
+const useStyles = makeStyles(theme => ({
+  root: {
+    paddingTop: theme.spacing(10)
+  },
+  alert: {
+    position: "absolute",
+    top: 0,
+    width: "100%",
+    display: "flex",
+    justifyContent: "center",
+    zIndex: 50000
+  },
+  paper: {
+    marginTop: theme.spacing(6),
+    padding: theme.spacing(4, 4, 6, 4)
+  }
+}))
 
 export default function MemberForm() {
   const params = useParams()
+  const classes = useStyles()
 
   const {
     state: { user }
@@ -104,39 +116,55 @@ export default function MemberForm() {
   return (
     <div>
       {!list.active ? (
-        <Alert severity="error" style={alertStyle}>
+        <Alert severity="error" className={classes.alert}>
           List has been activated :(. Contact your admin for details
         </Alert>
       ) : (
-        <Paper>
-          <Typography id="form-title">Add To List</Typography>
-          <div style={{ marginBottom: 10 }}>
-            <form onSubmit={handleSubmit}>
-              <Grid container>
-                {list.fields.map((label, index) => (
-                  <Grid key={index} item xs={12} style={{ marginBottom: 20 }}>
-                    <TextField
-                      id={label}
-                      name={fieldName(label)}
-                      type={label.toLowerCase() === "email" ? "email" : "text"}
-                      label={label}
-                      style={{ textTransform: "capitalize" }}
-                      autoFocus={index === 0 ? true : false}
-                      value={form[fieldName(label)]}
-                      onChange={handleInput}
-                      fullWidth
-                    />
-                  </Grid>
-                ))}
-              </Grid>
-              <div style={{ float: "right" }}>
-                <Button variant="contained" color="primary" type="submit">
-                  Add
-                </Button>
-              </div>
-            </form>
-          </div>
-        </Paper>
+        <Container component="main" maxWidth="sm" className={classes.root}>
+          <Typography
+            align="center"
+            variant="h4"
+            component="h1"
+            id="form-title"
+          >
+            Add To List
+          </Typography>
+          <Paper className={classes.paper}>
+            <div style={{ marginBottom: 10 }}>
+              <form onSubmit={handleSubmit}>
+                <Grid container>
+                  {list.fields.map((label, index) => (
+                    <Grid key={index} item xs={12} style={{ marginBottom: 20 }}>
+                      <TextField
+                        id={label}
+                        name={fieldName(label)}
+                        type={
+                          label.toLowerCase() === "email" ? "email" : "text"
+                        }
+                        label={label}
+                        style={{ textTransform: "capitalize" }}
+                        autoFocus={index === 0 ? true : false}
+                        value={form[fieldName(label)]}
+                        onChange={handleInput}
+                        fullWidth
+                      />
+                    </Grid>
+                  ))}
+                </Grid>
+                <div style={{ float: "right" }}>
+                  <Button
+                    variant="contained"
+                    color="primary"
+                    type="submit"
+                    aria-label="Add your info"
+                  >
+                    Add
+                  </Button>
+                </div>
+              </form>
+            </div>
+          </Paper>
+        </Container>
       )}
     </div>
   )
